16380f8fdc22d559555ed70c3e1bd8628ad2004c
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-13 20:51:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,5 +8,5 @@ def my_collect(array)
   collection
 end
 
-my_collect(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |name|  name.split(" ").first }
 
